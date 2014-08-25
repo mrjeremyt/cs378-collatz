1c5070ad292f252d1b344398b62da4cc4b6b9d52
mrjeremyt <jthompson2214@gmail.com>
2014-08-25 08:03:01 -0500

    adding all the files

---
+++ Collatz.h
@@ -0,0 +1,63 @@
+// --------------------------
+// projects/collatz/Collatz.h
+// Copyright (C) 2014
+// Glenn P. Downing
+// --------------------------
+
+#ifndef Collatz_h
+#define Collatz_h
+
+// --------
+// includes
+// --------
+
+#include <iostream> // istream, ostream
+#include <utility>  // pair
+
+// ------------
+// collatz_read
+// ------------
+
+/**
+ * read two ints
+ * @param  r a  std::istream
+ * @return a pair of the two ints, otherwise a pair of zeros
+ */
+std::pair<int, int> collatz_read (std::istream&);
+
+// ------------
+// collatz_eval
+// ------------
+
+/**
+ * @param i the beginning of the range, inclusive
+ * @param j the end       of the range, inclusive
+ * @return the max cycle length in the range [i, j]
+ */
+int collatz_eval (int, int);
+
+// -------------
+// collatz_print
+// -------------
+
+/**
+ * print three ints
+ * @param w a std::ostream
+ * @param i the beginning of the range, inclusive
+ * @param j the end       of the range, inclusive
+ * @param v the max cycle length
+ */
+void collatz_print (std::ostream&, int, int, int);
+
+// -------------
+// collatz_solve
+// -------------
+
+/**
+ * read, eval, print loop
+ * @param r a std::istream
+ * @param w a std::ostream
+ */
+void collatz_solve (std::istream&, std::ostream&);
+
+#endif // Collatz_h
---
+++ thousand.java~
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class thousand {
+	
+
+	public static void main(String[] args) throws FileNotFoundException{
+
+		final int MAX_VALUE = 997;
+		final int FULL_VALUE = 999999;
+
+		int[] range = new int[]{100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 999999};
+
+		PrintWriter file = new PrintWriter(System.getProperty("user.home") + "/Desktop/cs373-collatz/collatz/jmt2939-RunCollatz.in");
+
+		int count = 0;
+
+
+		//i > j
+		file.println("100 50");
+		count++;
+
+		//i == j
+		file.print("500000 500000");
+		count++;
+
+		Random rand = new Random();
+		
+
+		for(int j = 0; j < range.length; j++){
+
+			for(int i = 0; i < 99; i++){
+
+				int alpha = rand.nextInt(range[j]) + 1;
+				int bravo = rand.nextInt(range[j]) + 1;
+
+				file.println();
+
+				file.print(bravo + " " + alpha);
+				count++;
+			}
+		}
+
+		while(count < 999){
+			int alpha = rand.nextInt(FULL_VALUE) + 1;
+			int bravo = rand.nextInt(FULL_VALUE) + 1;
+			file.println();
+
+			file.print(bravo + " " + alpha);
+			count++;
+			//System.out.println(count);
+		}
+		file.println();
+		file.print("1 999999");
+
+		file.close();
+	}
+}
